376d3e5139cb72018df4ff0f8f68b76a7f42f5f1
robeanobug <fakeemail@gmail.com>
2025-06-05 12:46:44 -0400

    initialize card with suit and rank

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,10 @@
 class Card
+  attr_reader :rank, :suit
+  RANKS = %w[Spades Hearts Diamonds Clubs]
+  SUITS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
 end
---
+++ spec/card.spec.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/card'
+
+describe 'Card' do
+  describe '#initialize' do
+    it 'should initialize a card with a suit and rank' do
+      card = Card.new('Ace', 'Spades')
+      expect(card).to be_a(Card)
+      expect(card).to_not be_nil
+    end
+  end
+  
+  describe '==' do
+    it 'should return true when 2 cards are equal'
+  end
+  describe 'to_value' do
+    it 'should return the value of the card'
+  end
+end
